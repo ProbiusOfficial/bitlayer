62edcd688c141d4bc068ab60636f5d880d537581
探姬 <41804496+ProbiusOfficial@users.noreply.github.com>
2024-03-19 16:15:39 +0000

    Update label for tutorial sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,7 +56,7 @@ const config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Bitlayer.Doc',
         },
         { to: '/blog', label: 'Blog', position: 'left' },
         {
